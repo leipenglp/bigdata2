7d73772f6965b98e7afa875bbcff7146ef5a68b1
leipenglp <39868152+leipenglp@users.noreply.github.com>
2018-06-03 17:30:28 +0800

    Update 天气情况.py

--- 天气情况.py
+++ 天气情况.py
@@ -1,10 +1,9 @@
-﻿# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 """
-Created on Sat Jun  2 15:18:24 2018
+Created on Sun Jun  3 08:48:49 2018
 
 @author: lenovo
 """
-
 import urllib.request as r
 city_pinyin=input("请输入城市拼音:")
 url="http://api.openweathermap.org/data/2.5/forecast?q={},cn&mode=json&lang=zh_cn&&APPID=6a67ed641c0fda8b69715c43518b6996&units=metric"
@@ -13,13 +12,25 @@ print(info)
 import json
 w=json.loads(info)
 
-all_data=[]
-for i in range(0,38,4):
-    date=w["list"][i]["dt_txt"]
-    temp=w["list"][i]["main"]["temp"]
-    temp_max=w["list"][i]["main"]["temp_max"]
-    temp_min=w["list"][i]["main"]["temp_min"]
-    pressure=w["list"][i]["main"]["pressure"]
-    test="日期"+str(date),"\n当前温度"+str(temp),"\n最高温度"+str(temp_max),"\n最低温度"+str(temp_min),"\n气压"+str(pressure)
-    all_data.append(test)
-print(all_data)
+a1=w["list"][0]["weather"][0]["description"]
+print("欢迎使用全球天气app")
+print("1.查看当前城市未来五天天气 2.查看其他城市天气 3.保存")
+menu=input("请输入菜单：")
+if menu=="1":
+    print("1.查看当前城市未来五天天气")
+    print("当前城市为:"+city_pinyin)
+    for i in range(0,6):
+        print("6月3日的天气为:"+str(w["list"][i]['weather'][0]['description'])+";最高温度为:"+str(w["list"][i]["main"]["temp_max"])+";最低温度为:"+str(w["list"][i]["main"]["temp_min"]))
+        for j in range(6,14):
+            print("6月4日的天气为:"+str(w["list"][j]['weather'][0]['description'])+";最高温度为:"+str(w["list"][j]["main"]["temp_max"])+";最低温度为:"+str(w["list"][j]["main"]["temp_min"]))
+    for i in range(14,22):
+            print("6月5日的天气为:"+str(w["list"][16]['weather'][0]['description'])+";最高温度为:"+str(w["list"][16]["main"]["temp"])+";最低温度为:"+str(w["list"][16]["main"]["temp_min"]))
+    for j in range(22,30):
+            print("6月6日的天气为:"+str(w["list"][28]['weather'][0]['description'])+";最高温度为:"+str(w["list"][28]["main"]["temp"])+";最低温度为:"+str(w["list"][28]["main"]["temp_min"]))
+    for i in  range(30,38):
+            print("6月7日的天气为:"+str(w["list"][35]['weather'][0]['description'])+";最高温度为:"+str(w["list"][35]["main"]["temp"])+";最低温度为:"+str(w["list"][35]["main"]["temp_min"]))
+    
+if menu=="2":
+    print("2.查看其他城市天气")
+if menu=="3":
+    print("3.保存")
