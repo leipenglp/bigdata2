cbd346bf863a25d3709c0ed62cddcef2087438d4
leipenglp <39868152+leipenglp@users.noreply.github.com>
2018-06-03 11:40:50 +0800

    第二次作业

---
+++ day2.py.py
@@ -0,0 +1,18 @@
+﻿# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  3 08:47:34 2018
+@author: lenovo
+"""
+
+import urllib.request as r
+url='http://api.openweathermap.org/data/2.5/weather?q={}&mode=json&units=metric&lang=zh_cn&APPID=6a67ed641c0fda8b69715c43518b6996'
+info=r.urlopen(url).read().decode('utf-8','ignore')
+print(info)
+
+#josn(str)转换成dict
+
+import json
+w=json.loads(info)
+w['weather'][0]['descripition']
+w['main']['temp']
+
